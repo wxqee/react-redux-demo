6643bf971f78e4718881e6f26b0de4fdcb1a5f83
Wang Xiaoqiang <xiaoqiang.wang@storehub.com>
2018-09-08 22:56:01 +0800

    add link good

--- src/LinkManager.js
+++ src/LinkManager.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { addLink } from "./actions/link-actions";
 
 class LinkManager extends Component {
-  handleSubmit = () => {
+  form = null;
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const link = {
+      label: this.form.label.value,
+      url: this.form.url.value,
+    };
+
+    this.props.addLink(link);
   };
 
   render() {
     return (
       <div className="LinkManager">
         <section>
-          <form onSubmit={this.handleSubmit}>
+          <form ref={dom => { this.form = dom; }} onSubmit={this.handleSubmit}>
             <input type='text' name='label' />
             <input type='url' name='url' />
             <input type='submit' value='Add' />
@@ -32,6 +43,8 @@ const mapStateToProps = state => ({
   links: state.links,
 });
 
-// const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  addLink: link => dispatch(addLink(link)),
+});
 
-export default connect(mapStateToProps)(LinkManager);
+export default connect(mapStateToProps, mapDispatchToProps)(LinkManager);
